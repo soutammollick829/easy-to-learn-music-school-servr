aeefc6f3833ced61225a27868caf822b67f95ec7
soutammolick829 <soutammollick829@gmail.com>
2023-06-08 23:56:40 +0600

    create instructors get api server

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-require('dotenv').config()
-const cors = require('cors');
+require("dotenv").config();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-
-//middleware 
+//middleware
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Music school is running')
-  })
-
-  
+app.get("/", (req, res) => {
+  res.send("Music school is running");
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster10.dn0f8be.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -25,24 +22,45 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const popularClassCollection = client
+      .db("musicSchool")
+      .collection("populerClass");
+
+    const instructorsClassCollection = client
+      .db("musicSchool")
+      .collection("instructors");
+
+    // popular classes get api
+    app.get("/populer-class", async (req, res) => {
+      const result = await popularClassCollection.find().toArray();
+      res.send(result);
+    });
+    // instructors get api
+    app.get("/instructors", async (req, res) => {
+      const result = await instructorsClassCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-  app.listen(port, () => {
-    console.log(`Music school running on port: ${port}`)
-  })
+app.listen(port, () => {
+  console.log(`Music school running on port: ${port}`);
+});
